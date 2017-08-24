116b038f8c086f924946ebd11399657873a19915
snir <Snirx1@gmail.com>
2017-08-24 19:38:20 +0300

    we can see the map!!!!!! snappp

--- ParkitApp/src/components/App.js
+++ ParkitApp/src/components/App.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 import { Header } from './common';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+      }
+
     render() {
-        return (
-            <View>
-                <Header txt="shriky" />
-                <MapView
-                initialRegion={{
+    return (
+        <MapView
+            style={ styles.map }
+            initialRegion={{
                 latitude: 37.78825,
                 longitude: -122.4324,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
-                }}
-            />
-          </View>
-        );
+            }}
+        />
+    );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      height: 100,
+      width: 100
+    },
+  });
+  
 
 export default App;
