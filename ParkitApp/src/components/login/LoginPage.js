0f735245da8243abbb2132b565c771f32f2adf35
Aviv Moshe <Aviv5515722@gmail.com>
2017-08-25 07:14:18 +0300

    removed facebook login

--- ParkitApp/src/components/login/LoginPage.js
+++ ParkitApp/src/components/login/LoginPage.js
@@ -4,19 +4,22 @@ import {
     View,
     TouchableOpacity,
     Text,
-    StyleSheet
+    StyleSheet,
+    TextInput
 } from 'react-native';
 import {bindActionCreators} from "redux";
-import FBSDK, { LoginManager } from 'react-native-fbsdk';
+//import FBSDK, { LoginManager } from 'react-native-fbsdk';
 import ParkitLogo from '../common/ParkitLogo';
 import {connect} from "react-redux";
+import {Button} from '../common/';
 
 class LoginPage extends Component {
     constructor(props, context){
         super(props, context)
 
         this.state = {
-            login: false
+            login: false,
+            text: 'Useless Placeholder'
         }
 
         this._fbAuth = this._fbAuth.bind(this);
@@ -25,19 +28,19 @@ class LoginPage extends Component {
     }
 
     _fbAuth() {
-      LoginManager.logInWithReadPermissions(['public_profile']).then(
-         function(result) {
-            if (result.isCancelled) {
-               alert('Login cancelled');
-            } else {
-               alert('Login success with permissions: '
-               +result.grantedPermissions.toString());
-            }
-         },
-         function(error) {
-            alert('Login fail with error: ' + error);
-         }
-      );
+    //   LoginManager.logInWithReadPermissions(['public_profile']).then(
+    //      function(result) {
+    //         if (result.isCancelled) {
+    //            alert('Login cancelled');
+    //         } else {
+    //            alert('Login success with permissions: '
+    //            +result.grantedPermissions.toString());
+    //         }
+    //      },
+    //      function(error) {
+    //         alert('Login fail with error: ' + error);
+    //      }
+    //   );
    }
 
    logUser(isLogged) {
@@ -45,11 +48,30 @@ class LoginPage extends Component {
         this.setState({login: this.props.login});
    }
 
+   pressed(){
+       console.log('pressed');
+   }
+
     render() {
         return (
         <View style={styles.container}>
             <ParkitLogo />
             <Text style={styles.textBox}>App That Solves Your Problem </Text>
+            <TextInput
+                style={{height: 40, top:300}}
+                placeholder="UserName"
+                onChangeText={(text) => this.setState({text})}
+            />
+            <TextInput
+                style={{height: 40, top:320}}
+                placeholder="Password"
+                onChangeText={(text) => this.setState({text})}
+            />
+            <TouchableOpacity style={styles.contentLogin} onPress={this._fbAuth}>
+                <Text style={styles.textBoxLogin}>
+                    Login
+                </Text>
+            </TouchableOpacity>
             <TouchableOpacity style={styles.content} onPress={this._fbAuth}>
                 <Text style={styles.textBoxLogin}>
                     Login With Facebook
@@ -79,6 +101,20 @@ const styles = StyleSheet.create({
         paddingRight:20, 
         borderRadius:1
     },
+    contentLogin: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'column',
+        top: 380,
+        backgroundColor:'#4267b2',
+        width:300,
+        paddingTop:3,
+        minHeight: 30,
+        paddingBottom:20,
+        paddingLeft:20,
+        paddingRight:20, 
+        borderRadius:1
+    },
     textBoxLogin: {
         fontWeight:'bold',
         color:'#fff',
@@ -91,6 +127,10 @@ const styles = StyleSheet.create({
         fontSize:20,
         marginBottom:10,
         top: 300
+    },
+    textInput: {
+        width: 20,
+        height:10
     }
 });
 
